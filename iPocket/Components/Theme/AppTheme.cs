b529e284587092959c471f2b97f6bec3529f23f5
Nigzzzz <gabb09.perez@gmail.com>
2026-04-27 17:58:33 +0800

    new

---
+++ iPocket/Components/Theme/AppTheme.cs
@@ -0,0 +1,12 @@
+using System.Drawing;
+
+public static class AppTheme
+{
+    public static Color PrimaryPurple = Color.FromArgb(98, 0, 238);
+    public static Color DarkPurple = Color.FromArgb(30, 0, 60);
+    public static Color LightGray = Color.FromArgb(245, 245, 245);
+    public static Color White = Color.White;
+    public static Font TitleFont = new Font("Segoe UI", 18, FontStyle.Bold);
+    public static Font BodyFont = new Font("Segoe UI", 10, FontStyle.Regular);
+    public static Font ButtonFont = new Font("Segoe UI", 12, FontStyle.Bold);
+}
